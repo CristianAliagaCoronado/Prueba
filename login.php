d01ac5830f7af87bd6bf8722e3e5fe7855a80810
CristianAliagaCoronado <114546820+CristianAliagaCoronado@users.noreply.github.com>
2022-12-11 20:09:18 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,29 @@
 <?php
-$usuario='user';
-$contraseña='password';
+$correo=$_POST['correo'];
+$contraseña=$_POST['contraseña'];
+session_start();
+$_SESSION['correo']=$correo;
 
-?>
+
+$conexion=mysqli_connect("localhost", "root", "", "registro");
+
+$consulta=("SELECT * FROM usuario WHERE  correo='$correo' and contraseña='$contraseña'");
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+  
+    header("location: indexT.php");
+
+}else{
+    ?>
+    <?php
+    include("index.html");
+
+  ?>
+  <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
+  <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
